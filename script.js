accb0cb3665caa8e6a1eb6485f08691878717069
diego <diegoyon@outlook.com>
2022-06-21 13:39:28 -0600

    Fix linters errors

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 // MOBILE MENU
 const hamburger = document.querySelector('.mobile img');
 const body = document.querySelector('body');
-let menuitems = document.querySelectorAll('.desktop ul li a')
-menuitems = [...menuitems]
+let menuitems = document.querySelectorAll('.desktop ul li a');
+menuitems = [...menuitems];
 
-hamburger.addEventListener('click', displaymenu);
-
-function displaymenu(){
+function displaymenu() {
   const menu = document.createElement('div');
   menu.className = 'mobile-menu';
-  
+
   body.classList.add('noscroll');
 
   const iconx = document.createElement('img');
   iconx.className = 'iconx';
   iconx.src = './images/IconX.png';
-  iconx.addEventListener('click', ()=>{body.classList.remove('noscroll'); body.removeChild(menu)});
+  iconx.addEventListener('click', () => { body.classList.remove('noscroll'); body.removeChild(menu); });
   menu.appendChild(iconx);
 
-  for (let i=0; i<menuitems.length;  i++){
-    menuitems[i].addEventListener('click', ()=>{body.classList.remove('noscroll'); body.removeChild(menu)});
+  for (let i = 0; i < menuitems.length; i += 1) {
+    menuitems[i].addEventListener('click', () => { body.classList.remove('noscroll'); body.removeChild(menu); });
     menu.appendChild(menuitems[i]);
   }
 
   body.appendChild(menu);
 }
+
+hamburger.addEventListener('click', displaymenu);
 
 // DYNAMICALLY DISPLAY DJS
 const djs = [
@@ -66,9 +66,9 @@ const djs = [
   },
 ];
 
-const grid = document.querySelector('.grid-container')
+const grid = document.querySelector('.grid-container');
 
-for(let i=0; i<djs.length; i++){
+for (let i = 0; i < djs.length; i += 1) {
   const item = document.createElement('div');
   item.className = 'grid-item';
 
